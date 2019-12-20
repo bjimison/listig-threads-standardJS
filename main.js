e56efa497456f0f62ec2c3000d956a31c125fa69
bjimison <brady.t.jimison@gmail.com>
2019-12-20 12:52:12 -0800

    changed color scheme, img border radius, fixed button hover effects, and modal scrolling issue

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 // Modal section 
+
+// open camo modal
 document.getElementById("camo-btn").addEventListener("click", function () {
     document.querySelector(".bg-modal").style.display = "flex";
 });
 
+// open black modal
+document.getElementById("black-btn").addEventListener("click", function () {
+    document.querySelector(".bg-modal").style.display = "flex";
+});
+
+// open brown modal
+document.getElementById("brown-btn").addEventListener("click", function () {
+    document.querySelector(".bg-modal").style.display = "flex";
+});
+
+// close modal
 document.querySelector(".close").addEventListener("click", function () {
     document.querySelector(".bg-modal").style.display = "none";
 });
